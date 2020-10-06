5d58b2ce4c607d8676865b7f92535de07e705e32
Bengt Ahlgren <bengta@sics.se>
2020-10-06 20:50:58 +0200

    Implemented a generic class "NodeValueCSVWriter" that writes a csv-formatted table with a certain node value.  Each row contains the values for all nodes for a single point in time.

--- epanet-stepwise.py
+++ epanet-stepwise.py
@@ -2,12 +2,41 @@
 
 from epanet import toolkit as en
 import argparse
+import csv
 
 node_type_str = { en.JUNCTION: "junction",
                   en.RESERVOIR: "reservoir",
                   en.TANK: "tank" }
 
-def get_node_heads(ph, node_count):
+
+class NodeValueCSVWriter:
+    def __init__(self, csv_filename, project_handle, node_value):
+        self._csv_filename = csv_filename
+        self._ph = project_handle
+        self._node_value = node_value
+        node_count = en.getcount(self._ph, en.NODECOUNT)
+        self._node_range = range(1, node_count+1)
+
+    def __enter__(self):
+        self._f = open(self._csv_filename, 'w', newline='')
+        self._cw = csv.writer(self._f, delimiter=';')
+        self._cw.writerow(['time'] + [i for i in self._node_range])
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if (exc_type != None):
+            print(exc_type, exc_value, traceback)
+        self._f.close()
+        return False
+
+    def __call__(self, time):
+        self._cw.writerow([time] +
+                          [en.getnodevalue(self._ph, i, self._node_value)
+                           for i in self._node_range])
+
+
+def print_node_heads(ph, time, node_count):
+    print(f'Time: {time}')
     for i in range(1, node_count+1):
         node_type = en.getnodetype(ph, i)
         head = en.getnodevalue(ph, i, en.HEAD)
@@ -55,18 +84,26 @@ if __name__ == "__main__":
 
     print(f'Node count: {node_count}, link count: {link_count}')
 
-    ## Simulation loop
-    while True:
-        t = en.runH(ph)
-        #print(f'time after runH: {t}')
+    ## XXX - rewrite with contextlib.ExitStack
+    with NodeValueCSVWriter('pressure.csv', ph, en.PRESSURE) as pressure_wrt, \
+         NodeValueCSVWriter('head.csv', ph, en.HEAD) as head_wrt, \
+         NodeValueCSVWriter('demand.csv', ph, en.DEMAND) as demand_wrt:
 
-        get_node_heads(ph, node_count)
+        ## Simulation loop
+        while True:
+            t = en.runH(ph)
+            #print(f'time after runH: {t}')
 
-        hstep = en.nextH(ph)
-        #print(f'hstep after nextH: {hstep}')
+            #get_node_heads(ph, t, node_count)
+            pressure_wrt(t)
+            head_wrt(t)
+            demand_wrt(t)
 
-        if hstep == 0:
-            break
+            hstep = en.nextH(ph)
+            #print(f'hstep after nextH: {hstep}')
+
+            if hstep == 0:
+                break
 
     en.closeH(ph)
     #en.saveH(ph)
